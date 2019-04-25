c53d21be6f9f43d6aa3a9d3a526dde00c8960013
jinghaoo <hao.jing@huixaioer.com>
2019-04-25 20:53:12 +0800

    1

--- template/server/models/demo.js
+++ template/server/models/demo.js
@@ -1,8 +1,3 @@
-import CONFIG from '../config'
-import logger from '../libs/log4js'
-import { AESEncode, base64_urlSafeEncode } from '../utils'
-import KingApi from '../libs/HxerKongApi'
-
 export default class Demo {
     static getInstance () {
         if (!this.instance)
@@ -15,15 +10,7 @@ export default class Demo {
      * @param {*} uid 获取专员id
      */
     async getAdminInfo (uid) {
-        uid = base64_urlSafeEncode(AESEncode(CONFIG.ADMIN_UID_KEY, uid))
-        let params = { uid }
-        let kongApi = KingApi.getInstance('adminapi')
-        let outputData = await kongApi.kongCurl('/adminInfo', params)
 
-        if (2000 !== outputData.code || !outputData.data) {
-            logger.error('获取专员信息数据出错：input_data:' + JSON.stringify(params) + 'output_data:' + JSON.stringify(outputData))
-            return {}
-        }
 
         return outputData.data
     }
