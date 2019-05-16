f0efc46d86ae1f9a689ace0a46a37a5e1bc986a2
jinghaoo <hao.jing@huixaioer.com>
2019-05-16 18:41:10 +0800

    fetch

--- template/server/models/demo.js
+++ template/server/models/demo.js
@@ -1,17 +1,16 @@
 export default class Demo {
-    static getInstance () {
-        if (!this.instance)
-            this.instance = new Demo()
-        return this.instance
-    }
+  static getInstance () {
+    if (!this.instance) { this.instance = new Demo() }
+    return this.instance
+  }
 
-    /**
+  /**
      * 获取专员信息
      * @param {*} uid 获取专员id
      */
-    async getAdminInfo (uid) {
-
-
-        return outputData.data
+  async getAdminInfo (uid) {
+    return {
+      id: 1
     }
+  }
 }
--- template/server/utils/fetch.js
+++ template/server/utils/fetch.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from 'axios'
  
 //创建axios实例
 const service = axios.create({
-    timeout: 10000 //请求的超时时间
+    timeout: 5000 //请求的超时时间
 })
 
 service.interceptors.response.use(function (response) {
